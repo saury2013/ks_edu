c24652fca7047c4e4b7a12cf22e82a982e9349bd
saury2013 <saury2011@sina.com>
2018-04-20 21:10:07 +0800

    add uwsgi

--- ks_edu/settings.py
+++ ks_edu/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 
 STATIC_URL = '/static/'
 
